5f27ec999ee77060b4afb05d302efb6bd8cf6f0e
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-22 10:45:54 -0700

    implemented median and mode routes

--- app.js
+++ app.js
@@ -4,86 +4,109 @@ var request = require("request");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 // statistical operation
 const calcMean = (nums) => {
     let total = 0;
     for (let i = 0; i < nums.length; i++) {
-    total += nums[i];
+        total += nums[i];
     }
     mean = total / nums.length;
     return mean;
-}
+};
 
 const calcMedian = (nums) => {
     const mid = Math.floor(nums.length / 2),
-      numbers = nums.sort((a, b) => a - b);
-        median =
-          nums.length % 2 !== 0
+        numbers = nums.sort((a, b) => a - b);
+    median =
+        nums.length % 2 !== 0
             ? numbers[mid]
             : (numbers[mid - 1] + numbers[mid]) / 2;
-    return median
-}
+    return median;
+};
 
 const calcMode = (nums) => {
     var mode = {};
     var max = 0,
-    count = 0;
+        count = 0;
 
     nums.forEach(function (e) {
-      if (mode[e]) {
-        mode[e]++;
-      } else {
-        mode[e] = 1;
-      }
+        if (mode[e]) {
+            mode[e]++;
+        } else {
+            mode[e] = 1;
+        }
 
-      if (count < mode[e]) {
-        max = e;
-        count = mode[e];
-      }
+        if (count < mode[e]) {
+            max = e;
+            count = mode[e];
+        }
     });
 
     return max;
-}
+};
 
 const stringToArr = (nums) => {
-  let numsArr = nums.split(',').map(Number)
-  return numsArr
-}
+    let numsArr = nums.split(",").map(Number);
+    return numsArr;
+};
 
 // homepage route
-app.get("/", function (req, res) {
-  return res.send("Express Routing");
+app.get("/", function (request, res) {
+    return res.send("Express Routing");
 });
 
 // mean route
-app.get('/mean', (req, res) => {
-    let nums = request.query.nums
-    let numsArr = stringToArr(nums)
-    let meanResult = calcMean(numsArr)
-    
-    let result = {
-      operation: 'mean',
-      result: meanResult
-    }
-    res.send(result)
-})
+app.get("/mean", (request, res) => {
+    let nums = request.query.nums;
+    let numsArr = stringToArr(nums);
+    let meanResult = calcMean(numsArr);
 
+    let result = {
+        operation: "mean",
+        result: meanResult,
+    };
+    res.send(result);
+});
+
+// median route
+app.get("/median", (request, res) => {
+    let nums = request.query.nums;
+    let numsArr = stringToArr(nums);
+    let medianResult = calcMedian(numsArr);
+
+    let result = {
+        operation: "median",
+        result: medianResult,
+    };
+    res.send(result);
+});
+
+// mode route
+app.get("/mode", (request, res) => {
+    let nums = request.query.nums;
+    let numsArr = stringToArr(nums);
+    let modeResult = calcMode(numsArr);
+
+    let result = {
+        operation: "mode",
+        result: modeResult,
+    };
+    res.send(result);
+});
 
 // error handler
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message,
-  });
+    return res.json({
+        error: err,
+        message: err.message,
+    });
 });
 
-
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
